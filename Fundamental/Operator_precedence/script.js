32a9ecaa79daffd9ecf3caea6b455b5fb62c8df2
Majid <mjtaheri68@gmail.com>
2021-06-28 18:33:41 -0500

    operator_precedence

---
+++ Fundamental/Operator_precedence/script.js
@@ -0,0 +1,12 @@
+const now = 2037;
+const ageMajid = now - 1989;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageMajid + ageSarah) / 2;
+console.log(ageMajid, ageSarah, averageAge);
