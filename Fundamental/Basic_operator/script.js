3dccbf8931c7861f8199404d6df703a2edc8db53
Majid <mjtaheri68@gmail.com>
2021-06-28 18:16:31 -0500

    Assignment operators

--- Fundamental/Basic_operator/script.js
+++ Fundamental/Basic_operator/script.js
@@ -9,3 +9,13 @@ console.log(ageMajid * 2, ageSarah / 10, 2 ** 3);
 const firstName = 'Majid';
 const lastName = 'Taheri';
 console.log(firstName + ' ' +lastName);
+
+
+// Assignment operators
+let x = 100 + 50; // 150
+x += 100; // x = x + 100 = 250
+x *= 4; // x = x * 4 = 1000
+x++; // x = x + 1001
+x--;
+x--;
+console.log(x);
