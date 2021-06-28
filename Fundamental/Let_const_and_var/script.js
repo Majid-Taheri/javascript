4931134234a9eeeaf0ea78f7d753537d2984f4fa
Majid <mjtaheri68@gmail.com>
2021-06-28 17:12:59 -0500

    declare const

--- Fundamental/Let_const_and_var/script.js
+++ Fundamental/Let_const_and_var/script.js
@@ -1,4 +1,9 @@
-// let, const and var
+// mutate variable
 let age = 30;
 age = 31;
+
+// not suppose to change at any point in future immutable variable
+const birthYear = 1991;
+// birthYear = 1990; //doesnot work
+// const job; missiing initializer for const variable doesn't work
 
