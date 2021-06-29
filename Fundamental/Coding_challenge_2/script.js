25c5dfbc5a9c9a7de87eb6fbda0c89cfc2306adc
Majid <mjtaheri68@gmail.com>
2021-06-29 16:28:12 -0500

    solve coding chanllege with backsticks and placeholds 2

--- Fundamental/Coding_challenge_2/script.js
+++ Fundamental/Coding_challenge_2/script.js
@@ -19,4 +19,6 @@ if (markHighBmi) {
 
 if (markHighBmi) {
     console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}`);
+} else {
+    console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}`);
 }
