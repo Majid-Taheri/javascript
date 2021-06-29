b1dfa228ff52e6b7a709f02cb468656b191c46d5
Majid <mjtaheri68@gmail.com>
2021-06-29 15:37:12 -0500

    with placeholder

--- Fundamental/String_and_template_literals/script.js
+++ Fundamental/String_and_template_literals/script.js
@@ -1,7 +1,12 @@
 const firstName = 'Majid';
-const job = 'teacher';
+const job = 'engineer';
 const birthYear = 1989;
 const year = 2037;
 
-const Majid = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
-console.log(Majid);
+const majid = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
+console.log(majid);
+
+// template literals
+
+const majidNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(majidNew);
