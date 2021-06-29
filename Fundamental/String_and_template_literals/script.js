b66fc874d5071bed4b310f8cb3c588439903b110
Majid <mjtaheri68@gmail.com>
2021-06-29 15:38:36 -0500

    multiline strings

--- Fundamental/String_and_template_literals/script.js
+++ Fundamental/String_and_template_literals/script.js
@@ -10,3 +10,11 @@ console.log(majid);
 
 const majidNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
 console.log(majidNew);
+
+console.log(`Just a regular string...`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+
