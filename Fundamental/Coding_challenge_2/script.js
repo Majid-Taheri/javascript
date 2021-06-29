86c1eeee71e956325eda83fc134944075eaa2e71
Majid <mjtaheri68@gmail.com>
2021-06-29 16:20:45 -0500

    solve coding chanllege

---
+++ Fundamental/Coding_challenge_2/script.js
@@ -0,0 +1,18 @@
+const markWeight = 78;
+const markHeight = 1.69;
+const johnWeight = 92;
+const johnHeight = 1.95
+
+const markBMI =  markWeight/(markHeight*markHeight);
+console.log(markBMI)
+const johnBMI = johnWeight / (johnHeight**2);
+console.log(markBMI, johnBMI)
+
+const markHighBmi = markBMI > johnHeight;
+console.log(markBMI,johnBMI,markHighBmi);
+
+if (markHighBmi) {
+    console.log(`Mark's BMI is higher than John`);
+} else{
+    console.log(`John's BMI is higher than Mark`);
+}
