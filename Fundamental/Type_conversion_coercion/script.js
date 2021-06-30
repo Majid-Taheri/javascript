89cff926e04544f34161fe98dc4ea7660b67c656
Majid <mjtaheri68@gmail.com>
2021-06-30 14:34:41 -0500

    impossible conversion

--- Fundamental/Type_conversion_coercion/script.js
+++ Fundamental/Type_conversion_coercion/script.js
@@ -2,3 +2,6 @@ const inputYear = '1989';
 console.log(Number(inputYear), inputYear);
 console.log(Number(inputYear) + 18);
 
+console.log(Number('Majid')); // impossible to convert
+console.log(typeof NaN);
+console.log(typeof Number('Majid'));
