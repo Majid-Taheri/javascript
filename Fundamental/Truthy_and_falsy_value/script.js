76de3e79697288b9f0de490af0c849ba0788df8d
Majid <mjtaheri68@gmail.com>
2021-06-30 15:09:40 -0500

    truthy values in if condition

--- Fundamental/Truthy_and_falsy_value/script.js
+++ Fundamental/Truthy_and_falsy_value/script.js
@@ -4,5 +4,20 @@ console.log(Boolean(undefined));
 console.log(Boolean(''));
 
 //truthy values
-console.log(Boolean('MAjid'));
+console.log(Boolean('Majid'));
 console.log(Boolean({}));
+
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log('You should get a job!');
+}
+
+let height = 0;
+if (height) {
+  console.log('YAY! Height is defined');
+} else {
+  console.log('Height is UNDEFINED');
+}
