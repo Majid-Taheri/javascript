c5eb92204fd65ab75779f532d81dcea7b1be3143
Majid <mjtaheri68@gmail.com>
2021-07-01 22:17:16 -0500

    Swith case

---
+++ Fundamental/Swich_case_statement/script.js
@@ -0,0 +1,26 @@
+const day = 'friday';
+
+switch (day) {
+    case 'monday':
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday' :
+    case 'thursday' :
+        console.log("write code examples");
+        break;
+    case 'friday' :
+        console.log('Test your code');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log("Enjoy the weekend :smile:");
+        break;
+    default:
+        console.log('Not a valid day!')
+}
+
+
