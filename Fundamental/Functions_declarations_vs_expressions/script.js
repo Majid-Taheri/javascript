d3e2a8e4f339fe476f9d8d3e4261a846bdd9a285
Majid <mjtaheri68@gmail.com>
2021-07-02 17:43:29 -0500

    anonymus function

--- Fundamental/Functions_declarations_vs_expressions/script.js
+++ Fundamental/Functions_declarations_vs_expressions/script.js
@@ -1,8 +1,20 @@
 // Function declaration
 function calcAge1(birthYeah) {
     return 2037 - birthYeah;
-  }
-  const age1 = calcAge1(1989);
+}
+const age1 = calcAge1(1989);
+  
+  // Function expression
+  const calcAge2 = function (birthYeah) {
+    return 2037 - birthYeah;
+}
+const age2 = calcAge2(1989);
+  
+console.log(age1, age2);
   
 
-  
+const x = function (y) {
+    return y+1;
+}
+
+console.log (x (12));
