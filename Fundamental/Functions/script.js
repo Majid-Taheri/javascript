1c735b8123cb60f8221c589a18ba82a332d40919
Majid <mjtaheri68@gmail.com>
2021-07-02 17:24:43 -0500

    call a function

--- Fundamental/Functions/script.js
+++ Fundamental/Functions/script.js
@@ -1,9 +1,8 @@
-// The Conditional (Ternary) Operator
-let  age = 23;
-age >= 21 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+// Functions
+const myName = 'Majid';
+function logger () {
+    console.log (`My name is ${myName}`);
+}
 
-const drink = age>=18 ? 'wine🍷 ' : 'watermelon 🍉';
-
-console.log(drink);
-
-console.log(`I like to drink ${age >=18 ? 'wine🍷' : 'watermelon 🍉'}`);
+//calling / running/invoking function
+logger();
