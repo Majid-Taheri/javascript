b5f78a3eb47e95bc2fea76ccc5ac4452fad2d2aa
Majid <mjtaheri68@gmail.com>
2021-07-01 22:35:47 -0500

    short handed if/else in log statement

--- Fundamental/Conditional_operator/script.js
+++ Fundamental/Conditional_operator/script.js
@@ -1,3 +1,9 @@
 // The Conditional (Ternary) Operator
-const age = 23;
+let  age = 23;
 age >= 21 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+
+const drink = age>=18 ? 'wine🍷 ' : 'watermelon 🍉';
+
+console.log(drink);
+
+console.log(`I like to drink ${age >=18 ? 'wine🍷' : 'watermelon 🍉'}`);
