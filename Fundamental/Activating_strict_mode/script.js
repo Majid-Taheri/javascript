f046926407e1ef0f199081372c35ef7a968952c0
Majid <mjtaheri68@gmail.com>
2021-07-02 17:15:59 -0500

    example of some error in use strict

--- Fundamental/Activating_strict_mode/script.js
+++ Fundamental/Activating_strict_mode/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+let hasDriversLicense = false;
+const passTest = false;
+
+if (passTest) hasDriverLicense = true; //wrong variable missing 's' in hasDriverLicense
+if (hasDriversLicense) console.log('I can Drive 😃');
+
+const interface = 500; //error reserve keyword
+const if = 20; //error reserve keyword
