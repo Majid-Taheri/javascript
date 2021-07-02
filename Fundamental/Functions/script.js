038397e03af48862ff411023684e627500a28207
Majid <mjtaheri68@gmail.com>
2021-07-02 17:23:34 -0500

    call a function

---
+++ Fundamental/Functions/script.js
@@ -0,0 +1,9 @@
+// The Conditional (Ternary) Operator
+let  age = 23;
+age >= 21 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+
+const drink = age>=18 ? 'wine🍷 ' : 'watermelon 🍉';
+
+console.log(drink);
+
+console.log(`I like to drink ${age >=18 ? 'wine🍷' : 'watermelon 🍉'}`);
