530cce100c2e4b8ebf8def41cc961d838633d225
Majid <mjtaheri68@gmail.com>
2021-07-03 07:39:37 -0500

    arrow function

--- Fundamental/Arrow_function/script.js
+++ Fundamental/Arrow_function/script.js
@@ -1,13 +1,28 @@
 
 // Function expression
-const calcAge2 = function (birthYeah) {
-  return 2037 - birthYeah;
+const calcAge1 = function (birthYear) {
+  return 2037 - birthYear;
 }
-const age = calcAge2(1989);
+const age = calcAge1(1989);
 console.log(age);
 
 // Arrow Function
-const calcAge3 = birthYeah => 2037 - birthYeah;
+const calcAge3 = birthYear => 2037 - birthYear;
 const age1 = calcAge3(1989);
 console.log(age1);
 
+const yearUntilRetirement = birthYear => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return retirement;
+}
+
+console.log(yearUntilRetirement(1989));
+
+const yearsUntilRetirement = (birthYear, lastNAme) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return `${lastNAme} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1989,'Taheri'));console.log(yearsUntilRetirement(1980, 'Bob'));
