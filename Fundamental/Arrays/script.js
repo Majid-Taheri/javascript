1d7077fd025d1c1df8322f9ca55a4c922d548df1
Majid <mjtaheri68@gmail.com>
2021-07-06 12:59:16 -0500

    get actual number of elements that are in the array using  keyword

--- Fundamental/Arrays/script.js
+++ Fundamental/Arrays/script.js
@@ -9,3 +9,11 @@ console.log(friends);
 const y = new Array(1991, 1984, 2008, 2020);
 
 console.log(y);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+
