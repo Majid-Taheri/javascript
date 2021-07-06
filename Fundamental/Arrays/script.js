7e6238f2353778f6497312b8e0e7d88e0259c794
Majid <mjtaheri68@gmail.com>
2021-07-06 12:48:18 -0500

    creating arrays

---
+++ Fundamental/Arrays/script.js
@@ -0,0 +1,8 @@
+// Introduction to Arrays
+const friend1 = 'Michael';
+// const friend2 = 'Steven';
+// const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
