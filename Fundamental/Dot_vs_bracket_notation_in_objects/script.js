1979d9b8398356976f208288daae44bcc2c42717
Majid <mjtaheri68@gmail.com>
2021-07-08 16:43:19 -0500

    using prompt

--- Fundamental/Dot_vs_bracket_notation_in_objects/script.js
+++ Fundamental/Dot_vs_bracket_notation_in_objects/script.js
@@ -3,7 +3,7 @@ const majid = {
     firstName: 'Majid',
     lastName: 'Taheri',
     age: 2037 - 1989,
-    job: 'teacher',
+    job: 'engineer',
     friends: ['Michael', 'Peter', 'Steven']
   };
   console.log(majid);
@@ -14,3 +14,8 @@ const majid = {
   const nameKey = 'Name';
   console.log(majid['first' + nameKey]);
   console.log(majid['last' + nameKey]);
+
+
+const interestedIn = prompt('What do you want to know about Majid? Choose between firstName, lastName, age, job, and friends');
+
+console.log(interestedIn);
