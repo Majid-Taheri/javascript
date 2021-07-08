c3ee2158f190f214dfa7a2733857a428545484cc
Majid <mjtaheri68@gmail.com>
2021-07-07 22:08:43 -0500

    create list for bills and tips

--- Fundamental/Coding_challenge_6/script.js
+++ Fundamental/Coding_challenge_6/script.js
@@ -22,3 +22,9 @@ function calcTip (bill) {
         return 0.2 * bill;
     }
 }
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+console.log(tips);
