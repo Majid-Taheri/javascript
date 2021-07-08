005bee970bb501de7cc33bd6d6f328ed7e9d65d2
Majid <mjtaheri68@gmail.com>
2021-07-08 16:29:28 -0500

    initial commit

---
+++ Fundamental/Objects/script.js
@@ -0,0 +1,18 @@
+const majidArray = [
+    'Majid',
+    'Taheri',
+    2037 - 1989,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+  ];
+  
+
+  //object
+  //object has curly bracket and has key pair value
+  const majid = {
+    firstName: 'Majid',
+    lastName: 'Taheri',
+    age: 2037 - 1989,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+  };
