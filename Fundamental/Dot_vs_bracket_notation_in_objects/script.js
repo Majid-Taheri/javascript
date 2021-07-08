a2dbad57f0f514fcc1f44a92aa84b8b261f7eb4c
Majid <mjtaheri68@gmail.com>
2021-07-08 16:32:56 -0500

    grab the data with dot notation

---
+++ Fundamental/Dot_vs_bracket_notation_in_objects/script.js
@@ -0,0 +1,11 @@
+// Dot vs. Bracket Notation
+const majid = {
+    firstName: 'Majid',
+    lastName: 'Taheri',
+    age: 2037 - 1989,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+  };
+  console.log(majid);
+  
+  console.log(majid.lastName);
