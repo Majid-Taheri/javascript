2681bae07dfdcf5b39d1ed46ae0aec373bf998bf
Majid <mjtaheri68@gmail.com>
2021-07-10 08:37:54 -0500

    solve the challenge

--- Fundamental/Coding_challenge_7/script.js
+++ Fundamental/Coding_challenge_7/script.js
@@ -17,7 +17,8 @@ const mark = {
     mass : 78,
     height : 1.69,
     calcBMI : function() {
-       return this.bmi = this.mass / this.height **2; 
+       this.bmi = this.mass / this.height **2;
+       return this.bmi; 
     }
 };
 
@@ -26,8 +27,17 @@ const john = {
     mass : 92,
     height : 1.95,
     calcBMI : function () {
-        return this.bmi = this.mass / this.height **2;
+        this.bmi = this.mass / this.height **2;
+        return this.bmi;
     }
 };
+mark.calcBMI();
+john.calcBMI();
+console.log (mark.bmi,john.bmi);
 
-console.log (mark.calcBMI(),john.calcBMI());
+if (mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`)
+  } else if (john.bmi > mark.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+  }
+  
