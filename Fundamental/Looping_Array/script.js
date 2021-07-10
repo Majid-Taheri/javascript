029697c23e2363e2b2bffaaf95610eaf811dc66e
Majid <mjtaheri68@gmail.com>
2021-07-10 09:16:58 -0500

    using break and continue

--- Fundamental/Looping_Array/script.js
+++ Fundamental/Looping_Array/script.js
@@ -2,7 +2,7 @@ const majidArray = [
     'Majid',
     'Taheri',
     2037 - 1989,
-    'teacher',
+    'engineer',
     ['Michael', 'Peter', 'Steven']
 ];
 
@@ -21,3 +21,15 @@ for (let i = 0; i < years.length; i++) {
   ages.push(2037 - years[i]);
 }
 console.log(ages);
+
+// continue and break
+
+for (let i=0; i <= majidArray.length; i++) {
+    if (typeof majidArray[i] !== 'string' ) continue;
+    console.log(majidArray[i] , typeof majidArray[i]);
+}
+
+for (let i=0; i <= majidArray.length; i++) {
+    if (typeof majidArray[i] === 'number' ) break;
+    console.log(majidArray[i] , typeof majidArray[i]);
+}
