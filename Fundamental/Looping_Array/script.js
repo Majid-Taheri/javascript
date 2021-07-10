eb1ce4e7dd7c7d3a650159327a276c0b46108fd3
Majid <mjtaheri68@gmail.com>
2021-07-10 09:12:15 -0500

    create another array and fill it in the loop

--- Fundamental/Looping_Array/script.js
+++ Fundamental/Looping_Array/script.js
@@ -13,3 +13,11 @@ for (let i=0; i <= majidArray.length; i++) {
 }
 
 console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2037 - years[i]);
+}
+console.log(ages);
