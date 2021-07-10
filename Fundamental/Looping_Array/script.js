9854c5468a9ab1ff8c46313943dbc7254924fdd1
Majid <mjtaheri68@gmail.com>
2021-07-10 09:08:12 -0500

    create a for loop for an array

---
+++ Fundamental/Looping_Array/script.js
@@ -0,0 +1,12 @@
+const majidArray = [
+    'Majid',
+    'Taheri',
+    2037 - 1989,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+
+for (let i=0; i <= majidArray.length; i++) {
+    console.log(majidArray[i], typeof majidArray[i]);
+}
+  
