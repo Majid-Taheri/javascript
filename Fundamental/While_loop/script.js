d090bb42d1b1a41da9e13e73a87741c2e5e138a7
Majid <mjtaheri68@gmail.com>
2021-07-12 16:46:15 -0500

    initial commit

---
+++ Fundamental/While_loop/script.js
@@ -0,0 +1,10 @@
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+}
+
+let rep = 1;
+
+while (rep <= 10) {
+    console.log(`Lifting weights repetion ${rep} 🏋️`);
+    rep++;
+}
