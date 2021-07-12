91df43ff56163549789b4253e5b7b15d01e8c2a8
Majid <mjtaheri68@gmail.com>
2021-07-12 12:51:28 -0500

    initial commit

---
+++ Fundamental/Looping_backwards_and_loops_in_loops/script.js
@@ -0,0 +1,13 @@
+const majid = [
+    'Majid',
+    'Taheri',
+    2037 - 1989,
+    'engineer',
+    ['Michael', 'Peter', 'Steven']
+];
+
+for (let i = majid.length -1; i>=0 ; i--){
+    console.log(i,majid[i]);
+}
+
+  
